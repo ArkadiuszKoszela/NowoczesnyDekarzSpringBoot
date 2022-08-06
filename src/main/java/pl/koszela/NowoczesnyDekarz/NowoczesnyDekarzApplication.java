73536e8e513aced500d734d54a65d2190099b096
ArkadiuszKoszela <53365283+ArkadiuszKoszela@users.noreply.github.com>
2022-08-06 15:55:36 +0200

    Initial commit

---
+++ src/main/java/pl/koszela/NowoczesnyDekarz/NowoczesnyDekarzApplication.java
@@ -0,0 +1,13 @@
+package pl.koszela.NowoczesnyDekarz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowoczesnyDekarzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowoczesnyDekarzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/koszela/NowoczesnyDekarz/NowoczesnyDekarzApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koszela.NowoczesnyDekarz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowoczesnyDekarzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
